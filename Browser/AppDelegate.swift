e1041a6c192c0cb698c4636c4f968b676a457dde
Madalin Savoaia <madalin.savoaia@endava.com>
2019-07-04 15:50:56 +0300

    Initial commit

---
+++ Browser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Browser
+//
+//  Created by Madalin Savoaia on 04/07/2019.
+//  Copyright © 2019 Madalin Savoaia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Browser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by Madalin Savoaia on 04/07/2019.
+//  Copyright © 2019 Madalin Savoaia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
